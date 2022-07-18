324d1d2d7a302e636d471087154978483303fee7
ashurik522 <ashurik522@gmail.com>
2022-07-18 16:07:19 -0400

    get all and get by id

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,11 +1,22 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Account.getAll()
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = await Account.getById(req.params.id)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
@@ -21,7 +32,10 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
